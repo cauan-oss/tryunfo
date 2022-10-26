269c1375234b1cf7ec5fdc354614fd36d3995556
Cauan <cauanbarros14@gmail.com>
2022-10-26 16:47:55 -0300

    req 2 concluido

---
+++ src/components/Form.jsx
@@ -0,0 +1,107 @@
+const { Component } = require('react');
+
+class Form extends Component {
+  render() {
+    const { cardName, cardDescription, cardAttr1,
+      cardAttr2, cardAttr3, cardImage,
+      cardRare, cardTrunfo, isSaveButtonDisabled,
+      onInputChange, onSaveButtonClick } = this.props;
+    return (
+      <div>
+        <label htmlFor="nome-carta">
+          nome
+          <input
+            id="nome-carta"
+            data-testid="name-input"
+            value={ cardName }
+            onChange={ onInputChange }
+            type="text"
+          />
+        </label>
+        <label htmlFor="descricao-carta">
+          descricao
+          <input
+            id="descricao-carta"
+            type="textarea"
+            data-testid="description-input"
+            value={ cardDescription }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="atributo-carta1">
+          attr1
+          <input
+            id="atributo-carta1"
+            type="number"
+            data-testid="attr1-input"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="atributo-carta2">
+          attr2
+          <input
+            id="atributo-carta2"
+            type="number"
+            data-testid="attr2-input"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="atributo-carta3">
+          attr3
+          <input
+            id="atributo-carta3"
+            type="number"
+            data-testid="attr3-input"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="entrada-img">
+          imagem
+          <input
+            id="entrada-img"
+            type="text"
+            data-testid="image-input"
+            value={ cardImage }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="raridade-cart">
+          raridade
+          <select
+            data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
+          </select>
+        </label>
+        <label htmlFor="entarda-img">
+          Super trybe trunfo
+          <input
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+            id="entrada-img"
+            type="checkbox"
+            data-testid="trunfo-input"
+          />
+        </label>
+        <button
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+          type="button"
+          data-testid="save-button"
+        >
+          Salvar
+
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Form;

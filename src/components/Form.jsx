847ab89ca2f2c765800f862cd4c8314dfc7288cb
Cauan <cauanbarros14@gmail.com>
2022-10-26 19:50:21 -0300

    mais um req

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,13 @@
+import PropTypes from 'prop-types';
+
 const { Component } = require('react');
 
 class Form extends Component {
-
   render() {
     const { cardName, cardDescription, cardAttr1,
       cardAttr2, cardAttr3, cardImage,
-      cardRare, cardTrunfo, isSaveButtonDisabled,
-      onInputChange, onSaveButtonClick,  } = this.props;
+      cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
+      onInputChange, onSaveButtonClick } = this.props;
     return (
       <div>
         <label htmlFor="nome-carta">
@@ -105,5 +106,20 @@ class Form extends Component {
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
